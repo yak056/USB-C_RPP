bdbf8cd2a2a0b72e8c5618afd0d90fd4671b1cb9
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-06 15:13:20 +0100

    (+) Add canvas

--- www/scripts/pellicule.js
+++ www/scripts/pellicule.js
@@ -1,7 +1,7 @@
 const name = "img/img_";
 var tab_name = [];
-for (var i=1; i<14; i++){
-    tab_name.push(name+i+".JPG");
+for (var i=1; i<9; i++){
+    tab_name.push(name+i+".png");
 }
 console.log(tab_name);
 
@@ -10,8 +10,22 @@ function createPellicule(){
     for (var i=0; i<tab_name.length; i++){
         img.src = tab_name[i];
         const pellicule = document.getElementById("pellicule");
-        pellicule.innerHTML += '<div class="col-xs-2 img_pellicule" id="img_' + i + '"><img src="' + tab_name[i] + '"></div>';
+        pellicule.innerHTML += '<canvas class="col-xs-2 img_pellicule" id="img_' + i + '" ></canvas>';
     }
+    for (var i=0; i<tab_name.length; i++){
+        var canvas = document.getElementById("img_"+i);
+        var ctx = canvas.getContext('2d');
+        var image = new Image();
+        image.src = tab_name[i];
+        image.onload = draw_image(image, ctx);
+    }
+}
+
+function draw_image(image, ctx){
+    return function(){
+        ctx.drawImage(image, 0, 0);
+    }
+
 }
 
 createPellicule();
