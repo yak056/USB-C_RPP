222a5df1d7d48298de50782fae8443d3e949b90a
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-07 09:05:10 +0100

    (-) delete navbar1 --> navbar

--- www/scripts/navbar.js
+++ www/scripts/navbar.js
@@ -17,3 +17,14 @@ $('.navbar-primary').swipe( {
     }
 });
 
+function changeDiv(page){
+    document.getElementById('connexion_view').hidden= true;
+    document.getElementById('all_view').hidden= true;
+    document.getElementById('search_view').hidden= true;
+    document.getElementById('filter_view').hidden= true;
+    document.getElementById('resume_view').hidden= true;
+    document.getElementById('annotation_view').hidden= true;
+    document.getElementById('home_view').hidden= true;
+    document.getElementById(page).hidden = false;
+    console.log(page);
+}
