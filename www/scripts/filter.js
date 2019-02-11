44b9143f82bcbe86039ab88d2ad84d4b6fe577bc
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-11 23:21:06 +0100

    (+) Maj des balises sur les cotes RS lors du drag

--- www/scripts/filter.js
+++ www/scripts/filter.js
@@ -3,7 +3,6 @@ filter.pellicule = navigation.json;
 filter.init = function(){
     for (var i = 0; i < filter.pellicule.length; i++) {
         var graph = filter.pellicule[i];
-        print("hello + " + graph.bobo);
         if (graph.typeFilter == "numeric" || graph.typeFilter == "integer" || graph.typeFilter == "ordered"){
             filter.createFilterNumbered(graph);
         }
@@ -12,25 +11,25 @@ filter.init = function(){
         }
 
     }
-}
+};
 
 filter.createFilterNumbered = function(graph){
 
-}
+};
 filter.createFilterNamed = function(graph){
 
-}
+};
 $( function() {
     $( "#slider-range" ).slider({
         orientation: "horizontal",
         range: true,
         values: [ 17, 67 ],
         slide: function( event, ui ) {
-            $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+            document.getElementById("minRangeSlider").innerText = ui.values[ 0 ] ;
+            document.getElementById("maxRangeSlider").innerText = ui.values[ 1 ] ;
         }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    document.getElementById("minRangeSlider").innerText = $( "#slider-range" ).slider( "values", 0 )  ;
+    document.getElementById("maxRangeSlider").innerText = $( "#slider-range" ).slider( "values", 1 ) ;
 } );
-print(filter.pellicule);
 filter.init();
