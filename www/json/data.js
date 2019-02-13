ae96070381b5fa8be691cce9aa4bcc9c82917dec
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-13 15:22:01 +0100

    (~) refactor json

--- www/json/data.js
+++ www/json/data.js
@@ -5,13 +5,13 @@ var data =
     '       "typeFilter" : "integer",' +
     '       "min" : 1,' +
     '       "max" : 314,' +
-    '       "SD" : 91,67,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 86,25,' +
-    '       "3Q" : 242,75,' +
-    '       "mean" : 162,35,' +
-    '       "median" : 162,50,' +
-    '       "filterNames" : []' +
+    '       "sd" : 91.67,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 86.25,' +
+    '       "threeQ" : 242.75,' +
+    '       "mean" : 162.35,' +
+    '       "median" : 162.50,' +
+    '       "filterNames" : [],' +
     '       "filterEffectives" : []' +
     '       },' +
     '       {"nom" : "age",' +
@@ -19,13 +19,13 @@ var data =
     '       "typeFilter" : "integer",' +
     '       "min" : 18,' +
     '       "max" : 200,' +
-    '       "SD" : 10,91,' +
-    '       "NA" : 10,' +
-    '       "1Q" : 20,' +
-    '       "3Q" : 22,' +
-    '       "mean" : 21,57,' +
+    '       "sd" : 10.91,' +
+    '       "na" : 10,' +
+    '       "oneQ" : 20,' +
+    '       "threeQ" : 22,' +
+    '       "mean" : 21.57,' +
     '       "median" : 21,' +
-    '       "filterNames" : []' +
+    '       "filterNames" : [],' +
     '       "filterEffectives" : []' +
     '       },                ' +
     '       {"nom" : "poids",' +
@@ -33,13 +33,13 @@ var data =
     '       "typeFilter" : "integer",' +
     '       "min" : 46,' +
     '       "max" : 78,	' +
-    '       "SD" : 8,16,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 56,' +
-    '       "3Q" : 70,' +
+    '       "sd" : 8.16,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 56,' +
+    '       "threeQ" : 70,' +
     '       "mean" : 63.47,' +
     '       "median" : 66,' +
-    '       "filterNames" : []' +
+    '       "filterNames" : [],' +
     '       "filterEffectives" : []' +
     '       },' +
     '       {"nom" : "taille",' +
@@ -47,13 +47,13 @@ var data =
     '       "typeFilter" : "numeric",' +
     '       "min" : 1.59,' +
     '       "max" : 1.77,' +
-    '       "SD" : 0,04,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 1,65,' +
-    '       "3Q" : 1,71,' +
-    '       "mean" : 1,678,' +
-    '       "median" : 1,68,' +
-    '       "filterNames" : []' +
+    '       "sd" : 0.04,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 1.65,' +
+    '       "threeQ" : 1.71,' +
+    '       "mean" : 1.678,' +
+    '       "median" : 1.68,' +
+    '       "filterNames" : [],' +
     '       "filterEffectives" : []' +
     '       },' +
     '       {"nom" : "sexe",' +
@@ -61,13 +61,13 @@ var data =
     '       "typeFilter" : "nominal",' +
     '       "min" : null,' +
     '       "max" : null,' +
-    '       "SD" : 0,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 0,' +
-    '       "3Q" : 0,' +
+    '       "sd" : 0,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 0,' +
+    '       "threeQ" : 0,' +
     '       "mean" : 0,' +
     '       "median" : 0,' +
-    '       "filterNames" : [ "homme", "femme" ]' +
+    '       "filterNames" : [ "homme", "femme" ],' +
     '       "filterEffectives" : ["166", "120"]' +
     '       },' +
     '       {"nom" : "UFR",' +
@@ -75,13 +75,13 @@ var data =
     '       "typeFilter" : "nominal",' +
     '       "min" : null ,' +
     '       "max" : null,' +
-    '       "SD" : 0,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 0,' +
-    '       "3Q" : 0,' +
+    '       "sd" : 0,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 0,' +
+    '       "threeQ" : 0,' +
     '       "mean" : 0,' +
     '       "median" : 0,' +
-    '       "filterNames" : ["droit", "eco", "marketing", "piano", "politique", "sport"]' +
+    '       "filterNames" : ["droit", "eco", "marketing", "piano", "politique", "sport"],' +
     '       "filterEffectives" : ["35", "66", "10", "1", "28", "146"]' +
     '       },' +
     '       {"nom" : "redoublement",' +
@@ -89,13 +89,13 @@ var data =
     '       "typeFilter" : "nominal",' +
     '       "min" : null ,' +
     '       "max" : null,' +
-    '       "SD" : 0,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 0,' +
-    '       "3Q" : 0,' +
+    '       "sd" : 0,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 0,' +
+    '       "threeQ" : 0,' +
     '       "mean" : 0,' +
     '       "median" : 0,' +
-    '       "filterNames" : ["oui", "non"]' +
+    '       "filterNames" : ["oui", "non"],' +
     '       "filterEffectives" : ["143", "143"]' +
     '       },' +
     '       {"nom" : "scoreTricheTotal",' +
@@ -103,13 +103,13 @@ var data =
     '       "typeFilter" : "integer",' +
     '       "min" : 0,' +
     '       "max" : 28,' +
-    '       "SD" : 4,74,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 3,' +
-    '       "3Q" : 9,' +
-    '       "mean" : 6,283,' +
+    '       "sd" : 4.74,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 3,' +
+    '       "threeQ" : 9,' +
+    '       "mean" : 6.283,' +
     '       "median" : 5,' +
-    '       "filterNames" : []' +
+    '       "filterNames" : [],' +
     '       "filterEffectives" : []' +
     '       }, ' +
     '       {"nom" : "copier",' +
@@ -117,13 +117,14 @@ var data =
     '       "typeFilter" : "nominal",' +
     '       "min" : null,' +
     '       "max" : null,' +
-    '       "SD" : 0,' +
-    '       "NA" : 0,' +
-    '       "1Q" : 0,' +
-    '       "3Q" : 0,' +
+    '       "sd" : 0,' +
+    '       "na" : 0,' +
+    '       "oneQ" : 0,' +
+    '       "threeQ" : 0,' +
     '       "mean" : 0,' +
     '       "median" : 0,' +
-    '       "filterNames" : ["Assez souvent", "toujours", "Parfois", "Tres Rarement", "jamais"]' +
+    '       "filterNames" : ["Assez souvent", "toujours", "Parfois", "Tres Rarement", "jamais"],' +
     '       "filterEffectives" : ["36", "9", "99", "87", "54"]' +
     '       }' +
     ']}';
+console.log(data);
