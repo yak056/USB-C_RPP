7bdd2ca8f6f814487196fa4e19833f9124ac9ba4
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-13 15:46:21 +0100

    (+) Mise en forme resumé

--- www/scripts/resume.js
+++ www/scripts/resume.js
@@ -17,12 +17,12 @@ resume.createResumeForNumber = function(graph){
     table.innerHTML = "<tr><td id='mean'>mean</td><td>" + graph.mean + "</td></tr><tr><td id='sd'>sd</td><td>" + graph.sd +"</td></tr>"+
 "<tr><td id='min'>min</td><td>" + graph.min + "</td></tr><tr><td id='oneQ'>1Q</td><td>" + graph.oneQ + "</td></tr>" +
 "<tr><td id='median'>median</td><td>" + graph.median + "</td></tr><tr><td id='threeQ'>3Q</td><td>" + graph.threeQ + "</td></tr>"+
-"<tr><td id='max'>max</td><td>" +graph.max + "</td></tr><tr><td id='na'>NA</td><td>" + graph.NA + "</td></tr>";
+"<tr><td id='max'>max</td><td>" +graph.max + "</td></tr><tr><td id='na'>NA</td><td>" + graph.na + "</td></tr>";
 }
 resume.createResumeForNominal = function(graph){
     var table = document.getElementById("resume");
     table.innerText = "";
     for (var j = 0;  j < graph.filterEffectives.length; j++){
-        table.innerHTML += "<tr><td>" + graph.listFilters[i] + "</td><td>" + graph.filterEffectives[i] + "</td></tr>";
+        table.innerHTML += "<tr><td>" + graph.listFilters[j] + "</td><td>" + graph.filterEffectives[j] + "</td></tr>";
     }
 };
