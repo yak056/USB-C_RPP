344c437d3f4bdc351c79bb7e424b19dbfd3b3456
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-13 09:44:17 +0100

    (+) liste des filtres

--- www/scripts/filter.js
+++ www/scripts/filter.js
@@ -1,22 +1,20 @@
 var filter = {};
 filter.pellicule = navigation.pellicule.list;
 filter.init = function(){
-    for (var j=0; j<4; j++) {
-        for (var i = 0; i < filter.pellicule.length; i++) {
-            var graph = filter.pellicule[i];
-            if (graph.typeFilter == "numeric" || graph.typeFilter == "integer" || graph.typeFilter == "ordered") {
-                filter.createDivSlider(graph);
-            } else if (graph.typeFilter == "nominal" || graph.typeFilter == "logical") {
-                filter.createFilterNamed(graph);
-            }
-
+    for (var i = 0; i < filter.pellicule.length; i++) {
+        var graph = filter.pellicule[i];
+        if (graph.typeFilter == "numeric" || graph.typeFilter == "integer" || graph.typeFilter == "ordered") {
+            filter.createDivSlider(graph);
+        } else if (graph.typeFilter == "nominal" || graph.typeFilter == "logical") {
+            filter.createFilterNamed(graph);
         }
+
     }
 };
 
 filter.createDivSlider = function(graph){
     var filterlist = document.getElementById("filterList");
-    filterlist.innerHTML += " <div>\n" +
+    filterlist.innerHTML += " <div class=\"filterTitle\">\n" +
         "                                    <p>" + graph.name + "</p>\n" +
         "                                    <div class=\"col-xs-1\" id=\"minRangeSlider"+ graph.id + "\" style=\"text-align: right\">\n" +
         "                                        t1\n" +
@@ -31,25 +29,21 @@ filter.createDivSlider = function(graph){
     filter.createRangeSlider(graph.id, graph.min, graph.max);
 };
 
+filter.createCheckbox  = function (graph){
+    var innerHtml = "";
+    for (var i=0; i<graph.listFilters.length; i++){
+        innerHtml += "  <div class=\"form-check\" id=\"DivCheckBox" + graph.id + "_"+ i + "\">\n" +
+            "               <input type=\"radio\" id=\"CheckBox" + graph.id + "_"+ i + "\">\n" +
+            "               <label class=\"checkBoxLabel\" for=\"CheckBox" + graph.id + "_"+ i + "\">" + graph.listFilters[i] + "</label>\n" +
+            "           </div>";
+    }
+    return innerHtml;
+};
+
 filter.createFilterNamed = function(graph){
     var filterlist = document.getElementById("filterList");
-    filterlist.innerHTML += "<div>\n" +
-        "                                    <p>\n" +
-        "                                        My name 2\n" +
-        "                                    </p>\n" +
-        "                                    <div class=\"form-check\">\n" +
-        "                                        <input type=\"radio\" value=\"\" id=\"defaultCheck3\">\n" +
-        "                                        <label class=\"form-check-label\" for=\"defaultCheck3\">\n" +
-        "                                            Default checkbox\n" +
-        "                                        </label>\n" +
-        "                                    </div>\n" +
-        "                                    <div class=\"form-check\">\n" +
-        "                                        <input class=\"form-check-input\" type=\"radio\" value=\"\" id=\"defaultCheck4\">\n" +
-        "                                        <label class=\"form-check-label\" for=\"defaultCheck4\">\n" +
-        "                                            Disabled checkbox\n" +
-        "                                        </label>\n" +
-        "                                    </div>\n" +
-        "                                </div>";
+    filterlist.innerHTML += "<div class=\"filterTitle\"><p>" +  graph.name + "</p></div>";
+    filterlist.innerHTML += filter.createCheckbox(graph);
 };
 
 filter.createRangeSlider = function(id, min, max) {
