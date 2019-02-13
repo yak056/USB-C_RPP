b6ed1a19c590586b1267da359515a586f1c6540b
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-02-13 14:52:32 +0100

    (~) MAJ Json

--- www/json/data.js
+++ www/json/data.js
@@ -1,1 +1,129 @@
-var data = '{ "pellicule" : [{"nom" : "id",	"uri" : "img/id.png", "typeFilter" : "integer",	"min" : 1, "max" : 314,						"filterNames" : [																					]				} ,                {						"nom" : "age",						"uri" : "img/age.png",						"typeFilter" : "integer",						"min" : 18,						"max" : 200,						"filterNames" : [																					]				},                {						"nom" : "poids",						"uri" : "img/poids.png",						"typeFilter" : "integer",						"min" : 46,						"max" : 78,						"filterNames" : [																					]				},                {						"nom" : "taille",						"uri" : "img/taille.png",						"typeFilter" : "numeric",						"min" : 1.59,						"max" : 1.77,						"filterNames" : [																					]				} ,                {						"nom" : "sexe",						"uri" : "img/sexe.png",						"typeFilter" : "nominal",						"min" : null,						"max" : null,						"filterNames" : [ "homme", "femme" ]				},                {						"nom" : "UFR",						"uri" : "img/ufr.png",						"typeFilter" : "nominal",						"min" : null ,						"max" : null,						"filterNames" : [											"droit", "eco", "marketing", "piano"										]				},                {						"nom" : "redoublement",						"uri" : "img/redoublement.png",						"typeFilter" : "nominal",						"min" : null ,						"max" : null,						"filterNames" : [											"oui", "non"										]				},                {						"nom" : "scoreTricheTotal",						"uri" : "img/scoreTricheTotal.png",						"typeFilter" : "integer",						"min" : 0,						"max" : 28,						"filterNames" : [																					]				},                {						"nom" : "copier",						"uri" : "img/copier.png",						"typeFilter" : "nominal",						"min" : null,						"max" : null,"filterNames" : ["Assez souvent", "toujours", "Parfois", "Rarement", "jamais"]}]}'
+var data =
+    '{ "pellicule" : ' +
+    '   [   {"nom" : "id",' +
+    '       "uri" : "img/id.png",' +
+    '       "typeFilter" : "integer",' +
+    '       "min" : 1,' +
+    '       "max" : 314,' +
+    '       "SD" : 91,67,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 86,25,' +
+    '       "3Q" : 242,75,' +
+    '       "mean" : 162,35,' +
+    '       "median" : 162,50,' +
+    '       "filterNames" : []' +
+    '       "filterEffectives" : []' +
+    '       },' +
+    '       {"nom" : "age",' +
+    '       "uri" : "img/age.png",' +
+    '       "typeFilter" : "integer",' +
+    '       "min" : 18,' +
+    '       "max" : 200,' +
+    '       "SD" : 10,91,' +
+    '       "NA" : 10,' +
+    '       "1Q" : 20,' +
+    '       "3Q" : 22,' +
+    '       "mean" : 21,57,' +
+    '       "median" : 21,' +
+    '       "filterNames" : []' +
+    '       "filterEffectives" : []' +
+    '       },                ' +
+    '       {"nom" : "poids",' +
+    '       "uri" : "img/poids.png",' +
+    '       "typeFilter" : "integer",' +
+    '       "min" : 46,' +
+    '       "max" : 78,	' +
+    '       "SD" : 8,16,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 56,' +
+    '       "3Q" : 70,' +
+    '       "mean" : 63.47,' +
+    '       "median" : 66,' +
+    '       "filterNames" : []' +
+    '       "filterEffectives" : []' +
+    '       },' +
+    '       {"nom" : "taille",' +
+    '       "uri" : "img/taille.png",' +
+    '       "typeFilter" : "numeric",' +
+    '       "min" : 1.59,' +
+    '       "max" : 1.77,' +
+    '       "SD" : 0,04,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 1,65,' +
+    '       "3Q" : 1,71,' +
+    '       "mean" : 1,678,' +
+    '       "median" : 1,68,' +
+    '       "filterNames" : []' +
+    '       "filterEffectives" : []' +
+    '       },' +
+    '       {"nom" : "sexe",' +
+    '       "uri" : "img/sexe.png",	' +
+    '       "typeFilter" : "nominal",' +
+    '       "min" : null,' +
+    '       "max" : null,' +
+    '       "SD" : 0,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 0,' +
+    '       "3Q" : 0,' +
+    '       "mean" : 0,' +
+    '       "median" : 0,' +
+    '       "filterNames" : [ "homme", "femme" ]' +
+    '       "filterEffectives" : ["166", "120"]' +
+    '       },' +
+    '       {"nom" : "UFR",' +
+    '       "uri" : "img/ufr.png",' +
+    '       "typeFilter" : "nominal",' +
+    '       "min" : null ,' +
+    '       "max" : null,' +
+    '       "SD" : 0,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 0,' +
+    '       "3Q" : 0,' +
+    '       "mean" : 0,' +
+    '       "median" : 0,' +
+    '       "filterNames" : ["droit", "eco", "marketing", "piano", "politique", "sport"]' +
+    '       "filterEffectives" : ["35", "66", "10", "1", "28", "146"]' +
+    '       },' +
+    '       {"nom" : "redoublement",' +
+    '       "uri" : "img/redoublement.png",' +
+    '       "typeFilter" : "nominal",' +
+    '       "min" : null ,' +
+    '       "max" : null,' +
+    '       "SD" : 0,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 0,' +
+    '       "3Q" : 0,' +
+    '       "mean" : 0,' +
+    '       "median" : 0,' +
+    '       "filterNames" : ["oui", "non"]' +
+    '       "filterEffectives" : ["143", "143"]' +
+    '       },' +
+    '       {"nom" : "scoreTricheTotal",' +
+    '       "uri" : "img/scoreTricheTotal.png",' +
+    '       "typeFilter" : "integer",' +
+    '       "min" : 0,' +
+    '       "max" : 28,' +
+    '       "SD" : 4,74,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 3,' +
+    '       "3Q" : 9,' +
+    '       "mean" : 6,283,' +
+    '       "median" : 5,' +
+    '       "filterNames" : []' +
+    '       "filterEffectives" : []' +
+    '       }, ' +
+    '       {"nom" : "copier",' +
+    '       "uri" : "img/copier.png",' +
+    '       "typeFilter" : "nominal",' +
+    '       "min" : null,' +
+    '       "max" : null,' +
+    '       "SD" : 0,' +
+    '       "NA" : 0,' +
+    '       "1Q" : 0,' +
+    '       "3Q" : 0,' +
+    '       "mean" : 0,' +
+    '       "median" : 0,' +
+    '       "filterNames" : ["Assez souvent", "toujours", "Parfois", "Tres Rarement", "jamais"]' +
+    '       "filterEffectives" : ["36", "9", "99", "87", "54"]' +
+    '       }' +
+    ']}';
