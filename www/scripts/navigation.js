b6ed3e563b239cea944a71b4a35910525f12aa85
Claire COUSIN <claire.cousin@alumni.enac.fr>
2019-03-07 10:45:46 +0100

    (~) Comments + simplify

--- www/scripts/navigation.js
+++ www/scripts/navigation.js
@@ -1,8 +1,11 @@
 var navigation = {};
 
-navigation.onClickFilterCount = 0;
+navigation.onClickFilterCount = 0; // check if it is the first time we go to the filter page
+
+// #################### TOUCH HANDLER ###########################################
 
 navigation.touchHandler = function (event) {
+    // changes the handler in order to allow the speaker to draw on the canvas
     if (navigation.actualMainView == "BCC_img_for_annotation") {
         var touches = event.changedTouches,
             first = touches[0],
@@ -20,11 +23,6 @@ navigation.touchHandler = function (event) {
             default:
                 return;
         }
-
-        // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
-        //                screenX, screenY, clientX, clientY, ctrlKey, 
-        //                altKey, shiftKey, metaKey, button, relatedTarget);
-
         var simulatedEvent = document.createEvent("MouseEvent");
         simulatedEvent.initMouseEvent(type, true, true, window, 1,
             first.screenX, first.screenY,
@@ -34,33 +32,23 @@ navigation.touchHandler = function (event) {
         first.target.dispatchEvent(simulatedEvent);
         event.preventDefault();
     }
-}
+};
 
-navigation.init = function(){
+navigation.init = function () {
+    // changes touchmove event with the touchHandlers defined previously
     document.addEventListener("touchmove", navigation.touchHandler, true);
 };
 navigation.init();
-var print = console.log;
 
-navigation.actualMainView = "";
-navigation.json = JSON.parse(data).pellicule;
+// ###########################  NAVIGATION  ################################
 
-navigation.pellicule = new struct.Pellicule();
-
-navigation.changeJson = function(new_data){
-    var json = JSON.parse(new_data).pellicule;
-    navigation.json[3].uri = json[3].uri;
-    navigation.pellicule.list[3].uri = json[3].uri;
-    navigation.modifyVignette();
-    navigation.changeDiv('filter_view');
-};
+navigation.actualMainView = ""; // actual main view
+navigation.pellicule = null;
 
 var canvasList = ["img_home", "img_filter", "img_resume", "img_for_annotation"];
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////
 navigation.changeDivReal = function (page) {
+    // Change the page and init what should be init
     document.getElementById('connexion_view').hidden = true;
     document.getElementById('all_view').hidden = true;
     document.getElementById('filter_view').hidden = true;
@@ -72,34 +60,32 @@ navigation.changeDivReal = function (page) {
     else if (page == "filter_view") {
         navigation.actualMainView = "BCC_img_filter";
         navigation.onClickFilterCount++;
-        if (navigation.onClickFilterCount == 1){
+        if (navigation.onClickFilterCount == 1) {
             filter.init();
         }
-    }
-    else if (page == "resume_view") {
+    } else if (page == "resume_view") {
         navigation.actualMainView = "BCC_img_resume";
         resume.init()
     } else if (page == "annotation_view") {
         navigation.actualMainView = "BCC_img_for_annotation";
         toolBox.initCanvas(navigation.pellicule.currentIndex);
-    }
-    else if (page == "all_view") {
+    } else if (page == "all_view") {
         view_all.createAllViewList();
         view_all.sortPellicule();
-
     }
-    navbar.hidePellicule(page);
+    navbar.hidePellicule(page); // hide the pellicule if we have to hide it on page
 };
 
 navigation.changeDiv = function (page) {
+    // Change page and simulate a click on current graph
     navigation.changeDivReal(page);
     var currIndex = navigation.pellicule.currentIndex;
     navigation.simulEvent(document.getElementById("img_" + currIndex), "click");
 };
 
 
-
 navigation.simulEvent = function (idHTML, eventType) {
+    // Simulate event for loading the image when we change the menu selection (resume, annotation, ...)
     if (idHTML.fireEvent) {
         idHTML.fireEvent("on" + eventType)
     } else {
@@ -110,6 +96,7 @@ navigation.simulEvent = function (idHTML, eventType) {
 };
 
 navigation.initCanvasForPellicule = function (id, index, imgUrl) {
+    // Initialize the canvas for each graph for the pellicule
     var canvas = new fabric.Canvas(document.getElementById(id));
     var height = parseInt(getComputedStyle(document.getElementById("pellicule")).height);
     var width = parseInt(getComputedStyle(document.getElementById("pellicule")).width);
@@ -120,7 +107,6 @@ navigation.initCanvasForPellicule = function (id, index, imgUrl) {
     }, {
         cssOnly: false
     });
-    console.log(imgUrl);
     canvas.setBackgroundImage(
         imgUrl,
         canvas.renderAll.bind(canvas), {
@@ -131,13 +117,11 @@ navigation.initCanvasForPellicule = function (id, index, imgUrl) {
         });
     navigation.pellicule.list[index].vignetteCanvas = canvas;
     struct.duplicateAndResizeObjects(navigation.pellicule.list[index], canvas);
-
 };
 
 navigation.initCanvasForMainView = function (id, index, imgUrl) {
-
+    // Initialize the canvas for the main view
     imgUrl = navigation.pellicule.list[index].uri;
-
     var divContainer = document.getElementById("BCC_" + id);
     divContainer.innerHTML = "<canvas class=\"bigImg\" id=\"" + id + "\"></canvas>";
     var canvas = new fabric.Canvas(document.getElementById(id));
@@ -159,38 +143,30 @@ navigation.initCanvasForMainView = function (id, index, imgUrl) {
             backgroundImageStretch: true
         }
     );
-    //canvas.loadFromJSON(navigation.pellicule.list[index].drawingJson);
     struct.duplicateAndResizeObjects(navigation.pellicule.list[index], canvas);
-    if (id == "img_for_annotation")navigation.pellicule.list[index].designCanvas = canvas;
-    else navigation.pellicule.list[index].familyCanvas.push(canvas);
+
+    if (id == "img_for_annotation") navigation.pellicule.list[index].designCanvas = canvas;
+    else {
+        navigation.pellicule.list[index].familyCanvas.push(canvas);
+        canvas.selection = false;
+        canvas.forEachObject(function (o) {
+            o.selectable = false; // delete the selectable property on all canvas except the one from annotation
+        });
+    }
     document.getElementById(navigation.actualMainView).style.height = side + "px";
     document.getElementById(navigation.actualMainView).style.width = side + "px";
-
-    if(id != "img_for_annotation"){
-        canvas.selection = false;
-        canvas.forEachObject(function(o) {
-        o.selectable = false;
-        }); 
-    }
-
 };
 //////////////////////////////////////////////////////////////////////////////////////////////////
 navigation.createPellicule = function () {
-    /*for (var i = 0; i < navigation.json.length; i++) {
-        var graph = navigation.json[i];
-        navigation.pellicule.list.push(struct.createGraph(i, graph.uri, graph.nom, graph.typeFilter, graph.min,
-            graph.max, graph.mean, graph.sd, graph.oneQ, graph.median, graph.threeQ, graph.na, graph.filterEffectives, graph.filterNames));
-    }*/
+    // Create the pellicule with the graphs loaded from R++
     navigation.pellicule = rpp_connection.pellicule;
     const img = document.createElement("img");
-
     for (var i = 0; i < navigation.pellicule.list.length; i++) {
         img.src = navigation.pellicule.list[i].uri;
         const pelliculeHtml = document.getElementById("pellicule");
         var string = '<div id="vignetteContainer" style=" display:inline-block ; height=100% width=20%">' +
             '<canvas class="col-xs-2 img_pellicule"' + ' id="img_' + i + '" ></canvas></div>';
         pelliculeHtml.innerHTML += string;
-
     }
     for (var i = 0; i < navigation.pellicule.list.length; i++) {
         navigation.initCanvasForPellicule("img_" + i, i, navigation.pellicule.list[i].uri);
@@ -199,14 +175,8 @@ navigation.createPellicule = function () {
     }
 };
 
-navigation.draw_image = function (image, ctx) {
-    return function () {
-        var height = getComputedStyle(document.getElementById("pellicule")).height;
-        ctx.drawImage(image, 0, 0, parseInt(height) * 0.9, parseInt(height) * 0.9);
-    }
-};
-
 navigation.maj_img_canvas = function (img, index) {
+    // update the index, init Resume if needed and init the Canvas
     if (index != null) {
         navigation.pellicule.currentIndex = index;
     }
@@ -219,15 +189,16 @@ navigation.maj_img_canvas = function (img, index) {
     //to wrap canvas in a Fabric canvas
     if (navigation.actualMainView == "BCC_img_for_annotation")
         toolBox.initCanvas(index);
-    connection.sendPost();
 };
 navigation.changeImgMainView = function (img, index) {
+    // Closure to change the graph on the current view
     return function () {
         navigation.maj_img_canvas(img, index);
     }
 };
 
 navigation.onswipe = function (direction) {
+    // go to next or previous graph on swipe
     if (direction == "left") {
         var graph = navigation.pellicule.next();
         navigation.maj_img_canvas(graph.uri, navigation.pellicule.currentIndex);
@@ -244,19 +215,3 @@ $('.BigCanvasContainer').swipe({
         navigation.onswipe(direction);
     }
 });
-
-navigation.modifyVignette = function(){
-    var canvasPellicule = navigation.pellicule.list[3].vignetteCanvas;
-    canvasPellicule.setBackgroundImage(
-        navigation.pellicule.list[3].uri,
-        canvasPellicule.renderAll.bind(canvasPellicule), {
-            // Optionally add an opacity lvl to the image
-            backgroundImageOpacity: 1,
-            // should the image be resized to fit the container?
-            backgroundImageStretch: true
-        }
-    );
-};
-
-
-//navigation.createPellicule();
